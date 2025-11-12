d427aff48cca5a25f57347954da435bcf3b36e51
Felix Isael Mejia Castillo <isaelmejia06@gmail.com>
2025-11-12 01:07:36 -0400

    Creación del Ejercicio 5

---
+++ Actividad5LengProg3/Actividad5LengProg3/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Actividad5LengProg3.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
